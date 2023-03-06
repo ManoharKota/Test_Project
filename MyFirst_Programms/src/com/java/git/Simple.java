1aeba205365bf0d8fc913a877d2abdcaa1baabac
Admin <Admin@DESKTOP-7B1IAOO>
2023-03-06 14:35:49 +0530

    testing

--- MyFirst_Programms/src/com/java/git/Simple.java
+++ MyFirst_Programms/src/com/java/git/Simple.java
@@ -4,7 +4,7 @@ public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Git repositorssy");
+System.out.println("Hello Git ");
 	}
 
 }
