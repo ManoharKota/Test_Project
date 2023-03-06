4f625ef6029962dd85cfb46d0c36b910381c9381
Admin <Admin@DESKTOP-7B1IAOO>
2023-03-06 15:17:11 +0530

    method overloding

--- MyFirst_Programms/src/com/java/git/Simple.java
+++ MyFirst_Programms/src/com/java/git/Simple.java
@@ -2,9 +2,11 @@ package com.java.git;
 
 public class Simple {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello Git fttt ");
-	}
-
-}
+	static int add(int a,int b){return a+b;}  
+	static int add(int a,int b,int c){return a+b+c;}  
+	}  
+	class TestOverloading1{  
+	public static void main(String[] args){  
+	System.out.println(Simple.add(11,11));  
+	System.out.println(Simple.add(11,11,11));  
+	}}  
